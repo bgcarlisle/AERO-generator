b842cda19d7ceff296658323b8cdd60d349443a5
Benjamin Carlisle <murph@bgcarlisle.com>
2014-05-30 14:18:44 -0400

    Header and footer

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+     <title>AERO generator</title>
+
+     <!-- jQuery -->
+     <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js">
+     </script>
+     <!-- / jQuery -->
+
+     <!-- Google Fonts -->
+     <link href='http://fonts.googleapis.com/css?family=Lora|Lato' rel='stylesheet' type='text/css'>
+     <!-- / Google Fonts -->
+
+     <!-- AERO JS -->
+	<script type="text/javascript" src="<?php echo SITE_URL; ?>js/config.js">
+	</script>
+	<script type="text/javascript" src="<?php echo SITE_URL; ?>js/numbat.js">
+	</script>
+	<!-- / AERO JS -->
+
+     <!-- CSS -->
+     <link rel="stylesheet" href="<?php echo SITE_URL; ?>css/reset.css" />
+     <link rel="stylesheet" href="<?php echo SITE_URL; ?>css/aero.css" />
+     <!-- / CSS -->
+
+     <link rel="SHORTCUT ICON" href="<?php echo SITE_URL; ?>images/favicon.ico"/>
+
+</head>
+
+<body>
