759fe7fa11c9696794e273be9da4a702077bb5e1
Benjamin Carlisle <murph@bgcarlisle.com>
2014-07-24 14:56:05 -0400

    Link to start new AERO from editor

--- edit/edit.php
+++ edit/edit.php
@@ -4,7 +4,10 @@ $diagram = aero_get_diagram ( $_GET['id'] );
 
 $rows = aero_get_rows ( $_GET['id'] );
 
-?><div class="aeroDiagramEdit">
+?><div style="float: right; font-size: 12px; margin-right: 5%;">
+     <a href="<?php echo SITE_URL; ?>">Start a new AERO diagram</a>
+</div>
+<div class="aeroDiagramEdit">
      <h2>Diagram editor</h2>
      <p>Stratification label:</p>
      <input type="text" value="<?php echo $diagram['stratification_label']; ?>" id="aeroDiagramStratLabel" onblur="aeroUpdateDiagram(<?php echo $diagram['id'] ?>, 'stratification_label', 'aeroDiagramStratLabel');">
